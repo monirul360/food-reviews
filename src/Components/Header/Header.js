34046c92165c3401c10cc6badc8a2d2eff84f7b5
monirul360 <monirulislam65361@gmail.com>
2022-04-05 21:32:18 +0600

    responsive nav bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
+    const menu = () => {
+        const select = document.getElementById("reponsive-menu");
+        select.classList.toggle("onclickMenu");
+    }
     return (
         <div>
             <nav className='nav-content'>
-                <ul>
+                <div onClick={menu} className="menu">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <ul id='reponsive-menu'>
                     <li><Link to='/home'>home</Link></li>
                     <li><Link to='/reviews'>reviews</Link></li>
                     <li><Link to='/dashboard'>dashboard</Link></li>
